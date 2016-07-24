921842142a0e6c4b9fa360f23d88d229bee6207d
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-07-24 17:21:06 -0400

    Remove redundancy by ordering text operations

--- operational-transformation-lib/operational-transformation/text.rkt
+++ operational-transformation-lib/operational-transformation/text.rkt
@@ -12,14 +12,14 @@
   #:methods gen:operation
   [(define (transform-operation o1 o2)
      (operation-transformer (o1 o2)
-       [insertion? (transform-insertion-insertion o1 o2)]
+       [insertion? (order insertion-location transform-insertion-insertion o1 o2)]
        [deletion? (transform-insertion-deletion o1 o2)]))])
 
 (serializable-struct deletion (location length) #:transparent
   #:methods gen:operation
   [(define (transform-operation o1 o2)
      (operation-transformer (o1 o2)
-       [deletion? (transform-deletion-deletion o1 o2)]))])
+       [deletion? (order deletion-location transform-deletion-deletion o1 o2)]))])
 
 (define (string-perform-text-operation o d)
   (match o
@@ -34,31 +34,22 @@
      (string-append (substring d 0 lhs)
                     (substring d rhs))]))
 
-(define (transform-insertion-insertion o1 o2)
-  (match-define (insertion loc1 str1) o1)
-  (match-define (insertion loc2 str2) o2)
-  (if (> loc1 loc2)
-      (values (insertion (+ loc1 (string-length str2)) str1)
-              (insertion loc2 str2))
-      (values (insertion loc1 str1)
-              (insertion (+ loc2 (string-length str1)) str2))))
+(define (order a xf o1 o2)
+  (if (<= (a o1) (a o2))
+      (xf o1 o2)
+      (swap-values (xf o2 o1))))
 
-(define (transform-deletion-deletion o1 o2)
+(define (transform-insertion-insertion o1 o2) ;; Precondition: loc1 <= loc2
+  (match-define (insertion loc2 str2) o2)
+  (values o1 (insertion (+ loc2 (string-length (insertion-text o1))) str2)))
+
+(define (transform-deletion-deletion o1 o2) ;; Precondition: loc1 <= loc2
   (match-define (deletion loc1 len1) o1)
   (match-define (deletion loc2 len2) o2)
   (cond
-    [(>= loc2 (+ loc1 len1))  (values (deletion loc1 len1) (deletion (- loc2 len1) len2))]
-    [(>= loc1 (+ loc2 len2))  (values (deletion (- loc1 len2) len1) (deletion loc2 len2))]
-    [(>= loc1 loc2)
-     (if (<= (+ loc1 len1) (+ loc2 len2))
-         (values (deletion loc2 0) (deletion loc2 (- len2 len1)))
-         (let ((d (- (+ loc2 len2) loc1)))
-           (values (deletion loc2 (- len1 d)) (deletion loc2 (- len2 d)))))]
-    [else
-     (if (<= (+ loc2 len2) (+ loc1 len1))
-         (values (deletion loc1 (- len1 len2)) (deletion loc1 0))
-         (let ((d (- (+ loc1 len1) loc2)))
-           (values (deletion loc1 (- len1 d)) (deletion loc1 (- len2 d)))))]))
+    [(<= (+ loc1 len1) loc2)          (values (deletion loc1 len1) (deletion (- loc2 len1) len2))]
+    [(<= (+ loc2 len2) (+ loc1 len1)) (values (deletion loc1 (- len1 len2)) (deletion loc1 0))]
+    [else (values (deletion loc1 (- loc2 loc1)) (deletion loc1 (- (+ loc2 len2) (+ loc1 len1))))]))
 
 (define (transform-insertion-deletion o1 o2)
   (match-define (insertion loc1 str1) o1)
