55e84a280ca78e08a5c91af5c3caf1f5ed96148c
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-07-28 07:27:22 -0400

    Only send a requested filename if one is selected

--- operational-transformation-demo/multiclient.rkt
+++ operational-transformation-demo/multiclient.rkt
@@ -104,8 +104,9 @@
 
   (define (select-filename _widget _evt)
     (define selected-filename (send fns get-string-selection))
-    (send e enable #f)
-    (write-msg selected-filename o))
+    (when selected-filename
+      (send e enable #f)
+      (write-msg selected-filename o)))
 
   (define f (new frame%
                  [label "Racket OT Client"]
